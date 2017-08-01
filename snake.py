cef4984c7134c5f1fcc27ef8920bee0f41c5b324
mousa19-meet <mousa19@meet.mit.edu>
2017-08-01 14:26:26 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -10,7 +10,7 @@ turtle.setup(size_x,size_y)
 turtle.penup()
 
 square_size = 20
-start_len = 5
+start_len = 2
 
 pos_list =[]
 stamp_list = []
@@ -74,6 +74,7 @@ turtle.onkeypress(right_1,RIGHT_ARROW)
 turtle.onkeypress(left_1,LEFT_ARROW)
 turtle.listen()
 
+
 def move_snake():
     my_pos = snake.pos()
     x_pos = my_pos[0]
@@ -91,6 +92,7 @@ def move_snake():
     elif direction == DOWN:
         snake.goto(x_pos, y_pos - square_size)
         print('move down')
+        
     my_pos=snake.pos()
     pos_list.append(my_pos)
     new_stamp = snake.stamp()
@@ -105,38 +107,55 @@ def move_snake():
 
     if new_xpos >= right_edge:
         print('you hit the right edge , GAME OVER!')
-        #quit()
+        quit()
     elif new_xpos <= left_edge:
         print('you hit the left edge , GAME OVER!')
-        #quit()
+        quit()
     elif new_ypos >= up_edge:
         print('you hit the up edge, GAME OVER!')
-        #quit()
+        quit()
     elif new_ypos <= down_edge:
         print('you hit the down edge , GAME OVER!')
-        #quit()
+        quit()
     global food_stamps,fod_pos
     if snake.pos() in food_pos:
         food_ind = food_pos.index(snake.pos())
-        food.clearstamp(food_ind)
+        food.clearstamp(food_stamps[food_ind])
         food_pos.pop(food_ind)
-        fstamp.pop()
+        food_stamps.pop(food_ind)
         print('you ate food')
+        make_food()
+        stamp_list.append(stamp_me)
     turtle.ontimer(move_snake,TIME_STEP)
-move_snake()
-
+    
+    if pos_list[-1] == pos_list[0]:
+        quit()
 
 food = turtle.clone()
 food.shape('turtle')
 
-food_pos = [(100,100) , (-100,100) , (-100,-100) , (100,-100)]
+food_pos = []
 food_stamps = []
-for this_food_pos in food_pos:
-    food.goto(this_food_pos)
-    fstamp = food.stamp()
-    food_stamps.append(fstamp)
 
+
+def make_food():
     
+    min_x =-int(size_x/2/square_size)+1
+    max_x =int(size_x/2/square_size)-1
+    min_y=-int(size_y/2/square_size)-1
+    max_y=int(size_y/2/square_size)+1
+
+    food_x = random.randint(min_x,max_x)*square_size
+    food_y = random.randint(min_y,max_y)*square_size
+    food_tup=(food_x, food_y)
+    food_pos.append(food_tup)
+    food.goto(food_x,food_y)
+
+    food_stamp1 = food.stamp()
+    food_stamps.append(food_stamp1)
+     
+make_food()
+move_snake()
 
 
     
